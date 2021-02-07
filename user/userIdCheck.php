682c801fe05a9b5c5846f0758c32e69a4f96df8e
재키 <help@enas.kr>
2021-02-07 10:50:01 +0900

    아이디 중복 체크 오류 수정

--- user/userIdCheck.php
+++ user/userIdCheck.php
@@ -1,9 +1,5 @@
 <?php
-	$server_path = (str_replace(basename(__FILE__), "", realpath(__FILE__)));
-	$http_path = (str_replace(basename($_SERVER["PHP_SELF"]),"",$_SERVER["PHP_SELF"]));
-	include($server_path.'../lib/dbconn.php');
-	$cookieMbrNo = $_COOKIE["MBRNO"];
-	$webtoonDB = new SQLite3($server_path.'../lib/webtoon.db');
+	$webtoonDB = new SQLite3('../lib/webtoon.db');
 	if($webtoonDB->lastErrorCode() == 0){
 		if ( $USER_LEVEL == 99999 ) {
 			$userList = "SELECT MBR_NO, USER_ID, USER_PASSWD, USER_NAME, EMAIL, PHONE, USER_STATUS, REGDTIME, UPTDTIME FROM USER_INFO WHERE USER_ID = '".$_GET["userid"]."'; ";
