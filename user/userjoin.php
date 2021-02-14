a836e73251b3a79a614bef2a4bafb43740d4647d
재키 <help@enas.kr>
2021-02-14 14:36:03 +0900

    회원가입 오류수정

--- user/userjoin.php
+++ user/userjoin.php
@@ -4,12 +4,21 @@
 	$userList = "SELECT MBR_NO, USER_ID, USER_PASSWD, USER_NAME, EMAIL, PHONE, USER_STATUS, REGDTIME, UPTDTIME FROM USER_INFO WHERE USER_ID = '".$_POST["userid"]."'; ";
 	$webtoonView = $webtoonDB->query($userList);
 	$member_no = "";
+	$member_userid = $_POST["userid"];
 	while($row = $webtoonView->fetchArray(SQLITE3_ASSOC)){         
 		$memmbr_no = $row["MBR_NO"];
 		$memuserID = $row["USER_ID"];
 	}
 	if ( strlen($memmbr_no) == 0 ) {
-
+		if ( strlen(trim($member_userid)) == 0 ) {
+	?>
+	<script type="text/javascript">
+		alert("비정상적인 경로로 접근하였습니다.");
+		location.replace("<?php echo $homeurl; ?>");
+	</script>
+	<?php
+			return;
+		}
 		$usersql = "INSERT INTO 'USER_INFO' ('MBR_NO', 'USER_ID', 'USER_NAME', 'EMAIL', 'PHONE', 'USER_PASSWD', 'USER_STATUS', 'USER_LEVEL', 'USE_YN', 'REGDTIME', 'UPTDTIME') ";
 		if ( $USER_LEVEL == 99999 ) {
 			$usersql = $usersql." VALUES ('".$_POST["mbr_no"]."', '".$_POST["userid"]."','".$_POST["username"]."','".$_POST["useremail"]."','".$_POST["userphone"]."','".strtoupper(hash("sha256", $_POST["userpassword"]))."','".$_POST["userstatus"]."','".$_POST["userlevel"]."', 'Y', '".date("YmdHis", time())."', '".date("YmdHis", time())."');";
